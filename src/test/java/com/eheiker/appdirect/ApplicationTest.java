2d08e9bd1d7d65f86b5c3f67b3dc1a64fb6b593e
eheiker <eheiker@zinio.com>
2014-09-20 09:57:23 -0700

    fixed shading

--- src/test/java/com/eheiker/appdirect/ApplicationTest.java
+++ src/test/java/com/eheiker/appdirect/ApplicationTest.java
@@ -1,12 +1,19 @@
 package com.eheiker.appdirect;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 // https://github.com/spring-projects/spring-boot/blob/master/spring-boot-samples/spring-boot-sample-actuator-log4j/src/test/java/sample/actuator/log4j/SampleActuatorApplicationTests.java
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {Application.class})
 @DirtiesContext
 public class ApplicationTest {
 
+    @Test
+    public void test() {
+
+    }
 }
