31f8de7b10bd9efb496eef6278d172b1fd1c25bf
eheiker <eheiker@zinio.com>
2014-09-20 10:28:06 -0700

    testing using port 80

--- src/main/java/com/eheiker/appdirect/Application.java
+++ src/main/java/com/eheiker/appdirect/Application.java
@@ -1,10 +1,10 @@
 package com.eheiker.appdirect;
 
-import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +14,10 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Application {
 
+
+
     public static void main(String[] args) {
+
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         Logger log = LoggerFactory.getLogger(Application.class);
---
+++ src/main/java/com/eheiker/appdirect/web/WebConfig.java
@@ -0,0 +1,19 @@
+package com.eheiker.appdirect.web;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+
+        factory.setPort(80);
+
+        return factory;
+    }
+}
