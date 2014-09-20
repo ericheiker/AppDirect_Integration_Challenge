b44c2114875197786099a08e6395150536fe1b50
eheiker <eheiker@zinio.com>
2014-09-20 10:46:08 -0700

    made port configurable

--- src/main/java/com/eheiker/appdirect/web/WebConfig.java
+++ src/main/java/com/eheiker/appdirect/web/WebConfig.java
@@ -1,18 +1,27 @@
 package com.eheiker.appdirect.web;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.eheiker.appdirect.logging.AutowiredLogger;
 
 @Configuration
 public class WebConfig {
+
+    @Value("${local.server.port:8080}")
+    private int port;
 
     @Bean
     public EmbeddedServletContainerFactory servletContainer() {
         TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
 
-        factory.setPort(80);
+        factory.setPort(port);
 
         return factory;
     }
