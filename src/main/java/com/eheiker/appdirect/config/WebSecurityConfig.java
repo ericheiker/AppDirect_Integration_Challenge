77c2812a45fb81ed8bf08a6cd70b48d93d5f0303
eheiker <eheiker@zinio.com>
2014-09-24 11:15:49 -0700

    remove oauth

--- src/main/java/com/eheiker/appdirect/config/WebSecurityConfig.java
+++ src/main/java/com/eheiker/appdirect/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         //http
         //    .addFilterAfter(oAuthProviderProcessingFilter(), OpenIDAuthenticationFilter.class);
     }
-
+/*
     @Bean
     OAuthProviderProcessingFilter oAuthProviderProcessingFilter() {
         ProtectedResourceProcessingFilter filter = new ProtectedResourceProcessingFilter();
@@ -93,7 +93,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public OAuthProviderTokenServices providerTokenServices() {
         return new InMemoryProviderTokenServices();
     }
-
+*/
 
     private class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
         @Override
